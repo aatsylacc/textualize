c5b1cbf62c82332083a7a679e221f856810b0623
aatsylacc <lystahthania@gmail.com>
2024-12-03 14:11:47 +0700

    Remove all files

--- CC/Rest API/routes/ocr/route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const ocrHandler = require('../../routes/ocr/handler');
-const router = express.Router();
-
-// route buat handle request OCR
-router.post('/', ocrHandler);
-
-module.exports = router;
--- CC/Rest API/routes/summarize/route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const summarizeHandler = require('../../routes/summarize/handler');
-const router = express.Router();
-
-// route untuk handle request Summarize
-router.post('/', summarizeHandler);
-
-module.exports = router;
